a1b9a0d92f4964a0b0151199152e0da3219cbad5
anarakinson <anarakinson@gmail.com>
2025-06-11 21:12:21 +0300

    update 5.count

--- five_simple_tasks-01/5.count/main.go
+++ five_simple_tasks-01/5.count/main.go
@@ -13,7 +13,10 @@ countWords("  Extra   spaces   ") // 2 (игнорируем лишние про
 
 */
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func main() {
 
@@ -21,6 +24,8 @@ func main() {
 	fmt.Println(countWords("  Extra   spaces"))
 	fmt.Println(countWords("Extra spaces"))
 	fmt.Println(countWords("Extra  spaces      "))
+	fmt.Println(countWords("... Extra,   spaces !  "))
+	fmt.Println(countWords("..."))
 
 }
 
@@ -28,18 +33,27 @@ func countWords(str string) int {
 
 	var count int
 	var temp rune = 0
-	for i, ch := range str {
-		// если не начало строки, текущая руна равна " " и предыдущая не равна " "
-		if ch == ' ' && temp != ' ' && temp != 0 {
+	for i, r := range str {
+		// если не начало строки, текущая руна пробел или пункт. и предыдущая не пробел или пункт.
+		if space_or_punct(r) && !space_or_punct(temp) && temp != 0 {
 			// значит слово только закончилось
 			count += 1
-			// если конец строки и руна не равна " "
-		} else if ch != ' ' && i == len(str)-1 {
+			// если конец строки и руна не пробел или пункт.
+		} else if !space_or_punct(r) && i == len(str)-1 {
 			// значит слово только закончилось
 			count += 1
 		}
-		temp = ch
+		temp = r
 	}
 	return count
 
 }
+
+func space_or_punct(r rune) bool {
+	return r == ' ' || unicode.IsPunct(r)
+}
+
+// такое решение, на первый взгляд, проигрывает встроенным методам, например, strings.Fields()
+// тем не менее, подобные методы под капотом все равно используют итерацию по рунам строки.
+// в данном случае, мы избегаем потенциальной лишней работы при подсчете слов
+// и лишних аллокаций памяти при создании массивов.
